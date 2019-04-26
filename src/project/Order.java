2511921a26e9a3a77e1504105216bbed43745645
Juan <john256145@gmail.com>
2019-04-26 16:00:36 -0500

    removeOrder()

--- src/project/Order.java
+++ src/project/Order.java
@@ -7,10 +7,11 @@ public class Order
 	private int uoID; //Unique order ID (numeric)
 	private String cID; //Customer ID (alpha)
 	private String pID; //Product ID (alphanumeric)
-	private String date; // Date of the order
-	private String dateend; //End date of a repeated order
-	private int period; // the number of days between repeats (only for a repeated order)
+	private String date; // Date of the order	
 	private int amount; // Order amount
+	
+	private int period; // the number of days between repeats (only for a repeated order)
+	private String dateend; //End date of a repeated order
 	private boolean repeated; // specifies if the order is repeated or not
 	
 	//These will store all of the order information (IDs, dates, names, etc)
@@ -19,12 +20,10 @@ public class Order
 	private ArrayList <String> pIDList = new ArrayList <String>();
 	private ArrayList <String> dateList = new ArrayList <String>();
 	private ArrayList <Integer> amountList = new ArrayList <Integer>();
-	private ArrayList <Boolean> repeatedList = new ArrayList <Boolean>();
 	
-	
-	private ArrayList <String> dateendList = new ArrayList <String>();
 	private ArrayList <Integer> periodList = new ArrayList <Integer>();
-	
+	private ArrayList <String> dateendList = new ArrayList <String>();
+	private ArrayList <Boolean> repeatedList = new ArrayList <Boolean>();
 	
 	
 	public Order () {
@@ -33,12 +32,11 @@ public class Order
 		pID = "00";
 		date = "1/1/2019";
 		amount = 0;
-		repeated = false;
 		
 		
-		dateend = "0";
 		period = 0;
-		
+		dateend = "0";
+		repeated = false;
 	}
 	
 	public void addOrder(int uoID, String cID, String pID, String date, int amount) 
@@ -55,43 +53,64 @@ public class Order
 		pIDList.add(pID);
 		dateList.add(date);
 		amountList.add(amount);
-		repeatedList.add(false);
 		
-		dateendList.add(null);
 		periodList.add(0);
+		dateendList.add(null);
+		repeatedList.add(false);
 		
 	}
 	
-	public void addOrder (int uoID, String cID, String pID, String date, int amount, String dateend, int period) 
+	public void addOrder (int uoID, String cID, String pID, String date, int amount, int period, String dateend) 
 	{//adding a repeated order
 		this.uoID = uoID;
 		this.cID = cID;
 		this.pID = pID;
 		this.date = date;
 		this.amount = amount;
-		repeated = true;
-		this.dateend = dateend;
+		
 		this.period = period;
+		this.dateend = dateend;
+		repeated = true;
 		
 		uoIDList.add(uoID);
 		cIDList.add(cID);
 		pIDList.add(pID);
 		dateList.add(date);
 		amountList.add(amount);
-		repeatedList.add(true);
 		
-		dateendList.add(dateend);
 		periodList.add(period);
+		dateendList.add(dateend);
+		repeatedList.add(true);
 		
 	}
 	
-	public void deleteOrder (int uoID) 
+	public void deleteOrder (int ID) 
 	{
+		int index = 0;
+		int IDinarray;
+		for (int i = 0; i<uoIDList.size(); i++) 
+		{
+			IDinarray = uoIDList.get(i);
+			if (IDinarray == ID) 
+			{
+				index = i;
+				break;
+			}
+		}
 		
+		uoIDList.remove(index);
+		cIDList.remove(index);
+		pIDList.remove(index);
+		dateList.remove(index);
+		amountList.remove(index);
 		
-		
-		
-		
+		periodList.remove(index);
+		dateendList.remove(index);
+		repeatedList.remove(index);
 	}
+	
+	
+	
+	
 	
 }
--- src/project/OrderTest.java
+++ src/project/OrderTest.java
@@ -5,7 +5,22 @@ public class OrderTest
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		int uoID = 2569;
+		String cID = "Alphabet, Inc.";
+		String pID = "A1";
+		String date = "4/12/2019";
+		int amount = 1;
+		
+		int period = 0;
+		String dateend = "5/12/2019";
+		
+		Order myOrder = new Order();
+		
+		myOrder.addOrder(2569, "Alphabet, Inc.", "A1", "4/12/2019", 1);
+		myOrder.addOrder(2362, "Something Inc", "C2", "4/18/2019", 7);
+		myOrder.addOrder(8192, "Else, Inc.", "E2", "6/12/2019", 1, 2, "6/27/2019");
+		myOrder.addOrder(7623, "Idk, Inc.", "B2", "2/12/2019", 5, 2, "8/27/2019");
+		myOrder.addOrder(9109, "Company Inc", "A2", "3/18/2019", 9);
 
 	}
 
