50ccb4f37ae33672c8908f2e36005509401057e2
Juan <john256145@gmail.com>
2019-04-26 19:18:39 -0500

    fixed dates
    dates are no longer strings, they are now  stored as month and day integers

--- src/project/Order.java
+++ src/project/Order.java
@@ -7,22 +7,26 @@ public class Order
 	private int uoID; //Unique order ID (numeric)
 	private String cID; //Customer ID (alpha)
 	private String pID; //Product ID (alphanumeric)
-	private String date; // Date of the order	
+	private int month; // Date of the order	
+	private int day;
 	private int amount; // Order amount
 	
 	private int period; // the number of days between repeats (only for a repeated order)
-	private String dateend; //End date of a repeated order
+	private int monthend; //End date of a repeated order
+	private int dayend;
 	private boolean repeated; // specifies if the order is repeated or not
 	
 	//These will store all of the order information (IDs, dates, names, etc)
 	private ArrayList <Integer> uoIDList = new ArrayList <Integer>();
 	private ArrayList <String> cIDList = new ArrayList <String>();
 	private ArrayList <String> pIDList = new ArrayList <String>();
-	private ArrayList <String> dateList = new ArrayList <String>();
+	private ArrayList <Integer> monthList = new ArrayList <Integer>();
+	private ArrayList <Integer> dayList = new ArrayList <Integer>();
 	private ArrayList <Integer> amountList = new ArrayList <Integer>();
 	
 	private ArrayList <Integer> periodList = new ArrayList <Integer>();
-	private ArrayList <String> dateendList = new ArrayList <String>();
+	private ArrayList <Integer> monthendList = new ArrayList <Integer>();
+	private ArrayList <Integer> dayendList = new ArrayList <Integer>();
 	private ArrayList <Boolean> repeatedList = new ArrayList <Boolean>();
 	
 	
@@ -30,56 +34,65 @@ public class Order
 		uoID = 0;
 		cID = "Null, Inc,";
 		pID = "00";
-		date = "1/1/2019";
+		month = 00;
+		day = 0;
 		amount = 0;
 		
 		
 		period = 0;
-		dateend = "0";
+		monthend = 0;
+		dayend = 0;
 		repeated = false;
 	}
 	
-	public void addOrder(int uoID, String cID, String pID, String date, int amount) 
+	public void addOrder(int uoID, String cID, String pID, int month, int day, int amount) 
 	{//adding a one-time order
 		this.uoID = uoID;
 		this.cID = cID;
 		this.pID = pID;
-		this.date = date;
+		this.month = month;
+		this.day = day;
 		this.amount = amount;
 		repeated = false;
 		
 		uoIDList.add(uoID);
 		cIDList.add(cID);
 		pIDList.add(pID);
-		dateList.add(date);
+		monthList.add(month);
+		dayList.add(day);
 		amountList.add(amount);
 		
 		periodList.add(0);
-		dateendList.add(null);
+		monthendList.add(0);
+		dayendList.add(0);
 		repeatedList.add(false);
 		
 	}
 	
-	public void addOrder (int uoID, String cID, String pID, String date, int amount, int period, String dateend) 
+	public void addOrder (int uoID, String cID, String pID, int month, int day, int amount, int period, int monthend, int dayend) 
 	{//adding a repeated order
 		this.uoID = uoID;
 		this.cID = cID;
 		this.pID = pID;
-		this.date = date;
+		this.month = month;
+		this.day = day;
 		this.amount = amount;
 		
 		this.period = period;
-		this.dateend = dateend;
+		this.monthend = monthend;
+		this.dayend = dayend;
 		repeated = true;
 		
 		uoIDList.add(uoID);
 		cIDList.add(cID);
 		pIDList.add(pID);
-		dateList.add(date);
+		monthList.add(month);
+		dayList.add(day);
 		amountList.add(amount);
 		
 		periodList.add(period);
-		dateendList.add(dateend);
+		monthendList.add(monthend);
+		dayendList.add(dayend);
 		repeatedList.add(true);
 		
 	}
@@ -101,11 +114,13 @@ public class Order
 		uoIDList.remove(index);
 		cIDList.remove(index);
 		pIDList.remove(index);
-		dateList.remove(index);
+		monthList.remove(index);
+		dayList.remove(index);
 		amountList.remove(index);
 		
 		periodList.remove(index);
-		dateendList.remove(index);
+		monthendList.remove(index);
+		dayendList.remove(index);
 		repeatedList.remove(index);
 	}
 	
@@ -116,12 +131,14 @@ public class Order
 			System.out.print(uoIDList.get(i) + ", ");
 			System.out.print(cIDList.get(i) + ", ");
 			System.out.print(pIDList.get(i) + ", ");
-			System.out.print(dateList.get(i) + ", ");
+			System.out.print(monthList.get(i) + "/");
+			System.out.print(dayList.get(i) + "/2019, ");
 			System.out.print(amountList.get(i));
 			
 			if (repeatedList.get(i)) {
 				System.out.print(", " + periodList.get(i));
-				System.out.print(", " + dateendList.get(i));
+				System.out.print(", " + monthendList.get(i) + "/");
+				System.out.print(dayList.get(i) + "/2019");
 			}
 			
 			
@@ -130,28 +147,34 @@ public class Order
 		
 	}
 	
+	
 	public void listOrders(String cID)
 	{
+		
 		for (int i=0; i<cIDList.size(); i++)
 		{
 			if (cIDList.get(i) == cID) // if the customer order is found
 			{//print the order ( same code as printOrders() )
-				System.out.print(uoIDList.get(i) + ", ");
-				System.out.print(cIDList.get(i) + ", ");
-				System.out.print(pIDList.get(i) + ", ");
-				System.out.print(dateList.get(i) + ", ");
-				System.out.print(amountList.get(i));
 				
-				if (repeatedList.get(i)) {
-					System.out.print(", " + periodList.get(i));
-					System.out.print(", " + dateendList.get(i));
-				}
+			
 				
-				
-				System.out.println();
+//				System.out.print(uoIDList.get(i) + ", ");
+//				System.out.print(cIDList.get(i) + ", ");
+//				System.out.print(pIDList.get(i) + ", ");
+//				System.out.print(dateList.get(i) + ", ");
+//				System.out.print(amountList.get(i));
+//				
+//				if (repeatedList.get(i)) {
+//					System.out.print(", " + periodList.get(i));
+//					System.out.print(", " + dateendList.get(i));
+//				}
+//				
+//				
+//				System.out.println();
 			}
 			
 		}
+		
 		
 	}
 	
--- src/project/OrderTest.java
+++ src/project/OrderTest.java
@@ -5,31 +5,26 @@ public class OrderTest
 
 	public static void main(String[] args) 
 	{
-		int uoID = 2569;
-		String cID = "Alphabet, Inc.";
-		String pID = "A1";
-		String date = "4/12/2019";
-		int amount = 1;
 		
-		int period = 0;
-		String dateend = "5/12/2019";
 		
 		Order myOrder = new Order();
+							//Company ID, Product ID, month, day, amount.
+													//If a repeated order: period, month end, day end.
 		
-		myOrder.addOrder(2569, "Alphabet, Inc.", "A1", "4/12/2019", 1);
-		myOrder.addOrder(2362, "Something Inc", "C2", "4/18/2019", 7);
-		myOrder.addOrder(8192, "Alphabet, Inc.", "E2", "6/12/2019", 1, 2, "6/27/2019");
-		myOrder.addOrder(7623, "Idk, Inc.", "B2", "2/12/2019", 5, 2, "8/27/2019");
-		myOrder.addOrder(9109, "Alphabet, Inc.", "A2", "3/18/2019", 9);
 		
-//		myOrder.printOrders();
+		myOrder.addOrder(2569, "Alphabet, Inc.", "A1", 4, 12, 1);
+		myOrder.addOrder(2362, "Something Inc", "C2", 4, 18, 7);
+		myOrder.addOrder(8192, "Alphabet, Inc.", "E2", 6, 12, 1, 2, 6, 27);
+		myOrder.addOrder(7623, "Idk, Inc.", "B2", 2, 12, 5, 2, 8, 27);
+		myOrder.addOrder(9109, "Alphabet, Inc.", "A2", 3, 18, 9);
 		
-//		myOrder.deleteOrder(8192);
+		myOrder.printOrders();
+		
+		myOrder.deleteOrder(8192);
 		
 		System.out.println();
-//		myOrder.printOrders();
-		
-		myOrder.listOrders(cID);
+		myOrder.printOrders();
+
 	}
 
 }
